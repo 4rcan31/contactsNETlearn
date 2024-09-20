7a2972e085ec31dc7f77de08bb4bd8fd23af764a
4rcan31 <da.esfra12@gmail.com>
2024-09-19 19:09:46 -0600

    Execute entity framework

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using contactsNETlearn.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApiCsTestContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
